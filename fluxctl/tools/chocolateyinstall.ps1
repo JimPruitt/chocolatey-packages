cf1afcd2083190658fb3df46e9e2fd6f37346802
Jim Pruitt <jimpruitt775@gmail.com>
2018-11-13 20:43:14 -0500

    Made changes based on recommendations
    Changed 'author' and 'owner' specs. Fix 'projectSourceUrl'
    Added checksum to chocolateyInstall.ps1. Removed the
    unecessary use of Install-BinFile and consequently didn't
    need chocolateyUninstall.ps1 anymore. All changes
    recommended by the moderator.

--- fluxctl/tools/chocolateyinstall.ps1
+++ fluxctl/tools/chocolateyinstall.ps1
@@ -3,6 +3,8 @@
 #Generate the new filename based on what we're famliar with 
 $exeName = "fluxctl"
 $fileType   = "exe"
+$checkSum = "BC2061ED5A33C0DA3BA0A28D5AE762B94795EC7CAA3B4F877011B313F6CECE8D"
+$checkSumType = "SHA256"
 
 $toolsDir    = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir "$exeName.$fileType"
@@ -10,6 +12,4 @@ $fileLocation = Join-Path $toolsDir "$exeName.$fileType"
 $url        = 'https://github.com/weaveworks/flux/releases/download/1.8.0/fluxctl_windows_amd64'
 
 #Download the file from releases
-Get-ChocolateyWebFile $packageName $fileLocation $url
-
-Install-BinFile -Name $exeName -Path $fileLocation
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $fileLocation -Url $url -Checksum $checkSum -ChecksumType $checkSumType
--- fluxctl/tools/chocolateyuninstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-
-$exeName = "fluxctl"
-$fileType   = "exe"
-
-$toolsDir    = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$fileLocation = Join-Path $toolsDir "$exeName.$fileType"
-
-Uninstall-BinFile -Name $exeName -Path "$fileLocation"
