c926433ee763e7c37fe9d89b96c42477d69f73b7
James Pruitt <jpruitt@misoenergy.org>
2019-02-19 08:45:49 -0500

    Updated based on reviewers feedback.

--- fluxctl/tools/chocolateyinstall.ps1
+++ fluxctl/tools/chocolateyinstall.ps1
@@ -1,15 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-#Generate the new filename based on what we're famliar with 
-$exeName = "fluxctl"
-$fileType   = "exe"
-$checkSum = "A3EB5BB9BB9DDD5866EDDB7187D24EDB7AA827B44EEB84623744123ECE95A9D2"
-$checkSumType = "SHA256"
-
 $toolsDir    = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$fileLocation = Join-Path $toolsDir "$exeName.$fileType"
 
-$url        = 'https://github.com/weaveworks/flux/releases/download/1.10.1/fluxctl_windows_amd64'
+$xargs = @{
+    'PackageName'   ="fluxctl";
+    'FileFullPath'  = Join-Path $toolsDir "fluxctl.exe";
+    'Url'           = "https://github.com/weaveworks/flux/releases/download/1.10.1/fluxctl_windows_amd64";
+    'Checksum'      = "A3EB5BB9BB9DDD5866EDDB7187D24EDB7AA827B44EEB84623744123ECE95A9D2";
+    'ChecksumType'  = "SHA256"
+}
 
 #Download the file from releases
-Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $fileLocation -Url $url -Checksum $checkSum -ChecksumType $checkSumType
+Get-ChocolateyWebFile @xargs
