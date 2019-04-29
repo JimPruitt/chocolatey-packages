a8d22a33634cf3ca2bbc8fdbec3a18a75089b0d6
James Pruitt <jpruitt@midwestiso.org>
2019-04-29 08:03:59 -0400

    Updated to 1.12.1

--- fluxctl/tools/chocolateyinstall.ps1
+++ fluxctl/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $toolsDir    = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $xargs = @{
     'PackageName'   ="fluxctl";
     'FileFullPath'  = Join-Path $toolsDir "fluxctl.exe";
-    'Url'           = "https://github.com/weaveworks/flux/releases/download/1.11.1/fluxctl_windows_amd64";
-    'Checksum'      = "60FAB09359502C831C7184887F3141DA7784E3F4FBBBEA8F2A1EFC5A59A37AA9";
+    'Url'           = "https://github.com/weaveworks/flux/releases/download/1.12.1/fluxctl_windows_amd64";
+    'Checksum'      = "8FE04652C49AFDF40680423F73ED4A15D8D2632176AA04DAF00D4ECAFB69DC27";
     'ChecksumType'  = "SHA256"
 }
 
