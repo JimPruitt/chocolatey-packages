779f414030df0a25fed065480b55b786ffa3d3e8
James Pruitt <jimpruitt775@gmail.com>
2020-02-18 20:51:19 -0500

    Fluxctl upgraded to 1.18.0

--- fluxctl/tools/chocolateyinstall.ps1
+++ fluxctl/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $toolsDir    = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $xargs = @{
     'PackageName'   ="fluxctl";
     'FileFullPath'  = Join-Path $toolsDir "fluxctl.exe";
-    'Url'           = "https://github.com/weaveworks/flux/releases/download/1.17.0/fluxctl_windows_amd64";
-    'Checksum'      = "ABDB2FFA4B72343669145A576311A2C4788DC35CD0835D0A4727D57DA8C026B2";
+    'Url'           = "https://github.com/weaveworks/flux/releases/download/1.18.0/fluxctl_windows_amd64";
+    'Checksum'      = "ABECBD7A0CA7F79ABD3B219B5FD902A189D71A37104B41DD7E112614CFD9F2AF";
     'ChecksumType'  = "SHA256"
 }
 
