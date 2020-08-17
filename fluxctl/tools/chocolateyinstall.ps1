50944f78c924afed424de76af48a1ae916eb254a
James Pruitt <jimpruitt775@gmail.com>
2020-08-17 01:31:39 +0000

    Updating fluxctl package to version 1.20.1

--- fluxctl/tools/chocolateyinstall.ps1
+++ fluxctl/tools/chocolateyinstall.ps1
@@ -5,11 +5,11 @@ $ErrorActionPreference = 'Stop';
     $xargs = @{
         'PackageName'   ="fluxctl";
         'FileFullPath'  = Join-Path $toolsDir "fluxctl.exe";
-        'Url'           = "https://github.com/weaveworks/flux/releases/download/1.19.0/fluxctl_windows_amd64";
-        'Checksum'      = "FD4176A0F281AB1BEC557A66E47C0010253160914FC2C6E989C64208543AC1F7";
+        'Url'           = "https://github.com/weaveworks/flux/releases/download/1.20.1/fluxctl_windows_amd64";
+        'Checksum'      = "43A4880DFDDDFE644A74EEB936E73AA9D22C3397E90EE672ED15A387421760E2";
         'ChecksumType'  = "SHA256"
     }
     
     #Download the file from releases
-    Get-ChocolateyWebFile @xargs"
+    Get-ChocolateyWebFile @xargs
 
