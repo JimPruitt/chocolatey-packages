8adb730cc5b303de4b0d2e626ddd73210bf428b0
Kingdon Barrett <kingdon@weave.works>
2021-07-31 11:48:32 -0400

    Clean up weaveworks references
    It's fine to link to the weaveworks blog, but "Weaveworks Flux" should
    not be used anymore. 👍

--- fluxctl/tools/chocolateyinstall.ps1
+++ fluxctl/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $ErrorActionPreference = 'Stop';
     $xargs = @{
         'PackageName'   ="fluxctl";
         'FileFullPath'  = Join-Path $toolsDir "fluxctl.exe";
-        'Url'           = "https://github.com/weaveworks/flux/releases/download/1.23.1/fluxctl_windows_amd64";
+        'Url'           = "https://github.com/fluxcd/flux/releases/download/1.23.1/fluxctl_windows_amd64";
         'Checksum'      = "7FD953DF262B7CE93D84FD5FABC3C768C1DD62A09B8FABD6AD6478973E1E6F38";
         'ChecksumType'  = "SHA256"
     }
