44c727e8229145072d076c5fcdd911e19638ce07
James Pruitt <jimpruitt775@gmail.com>
2021-09-06 16:38:28 -0400

    Updated 'flux' to vresion 0.17.0. Added support for 64-bit executable. (#15)

--- flux2/tools/chocolateyinstall.ps1
+++ flux2/tools/chocolateyinstall.ps1
@@ -5,9 +5,12 @@ $ErrorActionPreference = 'Stop';
     $xargs = @{
         'PackageName'   ="flux";
         'Unziplocation' = $(Split-Path -Parent $MyInvocation.MyCommand.Definition)
-        'Url'           = "https://github.com/fluxcd/flux2/releases/download/v0.16.2/flux_0.16.2_windows_386.zip";
-        'Checksum'      = "8DBCB1B22487D1D9DF1FDC9782FA36C63A1CB6F15CA0F61E5F74C802B10A281F";
+        'Url'           = "https://github.com/fluxcd/flux2/releases/download/v0.17.0flux_0.17.0_windows_386.zip";
+        "Url64Bit"      = "https://github.com/fluxcd/flux2/releases/download/v0.17.0/flux_0.17.0_windows_amd64.zip"
+        'Checksum'      = "0B3E5DA440F8FEDE393C75A74F857018527C213D57A3E1EFE40ABA3A8CDA2D80";
+        'Checksum64'    = "4F4F9BF34E691D85BAD7B0752A0C5471EC665A69D538569DAAB2B6239A54D83C"
         'ChecksumType'  = "SHA256"
+        'ChecksumType64' = "SHA256"
     }
     
     #Download the file from releases
