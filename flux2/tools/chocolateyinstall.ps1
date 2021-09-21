e2d1fb87b46c59ccc2fa3e4dfea3cb3e429e91c6
James Pruitt <jimpruitt775@gmail.com>
2021-09-21 09:19:28 -0400

    Ugrading flux2 to version 0.17.2 (#18)

--- flux2/tools/chocolateyinstall.ps1
+++ flux2/tools/chocolateyinstall.ps1
@@ -5,10 +5,10 @@ $ErrorActionPreference = 'Stop';
     $xargs = @{
         'PackageName'   ="flux";
         'Unziplocation' = $(Split-Path -Parent $MyInvocation.MyCommand.Definition)
-        'Url'           = "https://github.com/fluxcd/flux2/releases/download/v0.17.1/flux_0.17.1_windows_386.zip";
-        "Url64Bit"      = "https://github.com/fluxcd/flux2/releases/download/v0.17.1/flux_0.17.1_windows_amd64.zip"
-        'Checksum'      = "72C1DAF7753B0C8F5BB5943E6A297AAF22F9365F2C8A0987D3138F8E603B090F";
-        'Checksum64'    = "BD73E1CF2D0F0EE0D42FBBAD2B810110834EC7952416D019CDFB72FA9B4212B2"
+        'Url'           = "https://github.com/fluxcd/flux2/releases/download/v0.17.2/flux_0.17.2_windows_386.zip";
+        "Url64Bit"      = "https://github.com/fluxcd/flux2/releases/download/v0.17.2/flux_0.17.2_windows_amd64.zip"
+        'Checksum'      = "8FBD5F147CA0BEEF338D9776538B6BDC966995D0F760B485DB860B08E4FB536D";
+        'Checksum64'    = "7020389B21E81089E11B155B8ED9B3A9DEF658A15777E919F1C78F7C19FAD527"
         'ChecksumType'  = "SHA256"
         'ChecksumType64' = "SHA256"
     }
