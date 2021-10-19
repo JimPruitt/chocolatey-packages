894b4c81302aaef298e53755d96fa2e3ba861641
Artem Vetrov <foxxconn@protonmail.com>
2021-10-19 20:52:24 +0300

    Fix 64-bit version executable (#22)
    Excellent catch @tsunamaru!   This appears to be a bug caused by the automated update process I'm working on.   I'll need to incorporate an additional test in that process to ensure that this doesn't happen again.

--- flux2/tools/chocolateyinstall.ps1
+++ flux2/tools/chocolateyinstall.ps1
@@ -6,9 +6,9 @@ $ErrorActionPreference = 'Stop';
         'PackageName'    ="flux";
         'Unziplocation'  = $(Split-Path -Parent $MyInvocation.MyCommand.Definition)
         'Url'            = "https://github.com/fluxcd/flux2/releases/download/v0.18.3/flux_0.18.3_windows_386.zip";
-        'Url64Bit'       = "https://github.com/fluxcd/flux2/releases/download/v0.18.3/flux_0.18.3_windows_386.zip"
+        'Url64Bit'       = "https://github.com/fluxcd/flux2/releases/download/v0.18.3/flux_0.18.3_windows_amd64.zip"
         'Checksum'       = "52AA9E6FD942F247BC317FE3D4758676CF8FB1FBF1139EDEEEA25FAAFC7EAD7C";
-        'Checksum64'     = "52AA9E6FD942F247BC317FE3D4758676CF8FB1FBF1139EDEEEA25FAAFC7EAD7C"
+        'Checksum64'     = "CCA203D1CAA46AA15E03DF1D1CCB9FAAA4D75D268A28CF83C472746A43F2C200"
         'ChecksumType'   = "SHA256"
         'ChecksumType64' = "SHA256"
     }
