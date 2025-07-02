16f15835a3b25381b81e0fe2478ca2021ec03b62
James Pruitt <jimpruitt775@gmail.com>
2025-07-02 15:05:59 +0000

    Updating flux2 package to version 2.6.3

--- flux2/tools/chocolateyinstall.ps1
+++ flux2/tools/chocolateyinstall.ps1
@@ -5,10 +5,10 @@ $ErrorActionPreference = 'Stop';
     $xargs = @{
         'PackageName'    ="flux";
         'Unziplocation'  = $(Split-Path -Parent $MyInvocation.MyCommand.Definition)
-        'Url'            = "https://github.com/fluxcd/flux2/releases/download/v2.6.2/flux_2.6.2_windows_386.zip";
-        'Url64Bit'       = "https://github.com/fluxcd/flux2/releases/download/v2.6.2/flux_2.6.2_windows_amd64.zip"
-        'Checksum'       = "E7BAAC5DA49ADFDB804453703CCCE33994E9BBD546B889F7E4DC832493EB5E9D";
-        'Checksum64'     = "A71D06D4B2DEA5218FB68A11DC417FCF6BBA1AC5AC17F1FE3272DB5EAD26C20F"
+        'Url'            = "https://github.com/fluxcd/flux2/releases/download/v2.6.3/flux_2.6.3_windows_386.zip";
+        'Url64Bit'       = "https://github.com/fluxcd/flux2/releases/download/v2.6.3/flux_2.6.3_windows_amd64.zip"
+        'Checksum'       = "CF5F96818FDEEAEDCC5A36868E228ACA7BD5D8115BF6B16574C974744C6529F8";
+        'Checksum64'     = "22931173FED8D64AFCFF86D78856EB45C3EA8A3B52EA74753CE563B2DA4663C3"
         'ChecksumType'   = "SHA256"
         'ChecksumType64' = "SHA256"
     }
